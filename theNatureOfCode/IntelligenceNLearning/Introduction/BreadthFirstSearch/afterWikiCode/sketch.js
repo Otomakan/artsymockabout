d50e975e2f5aeb3f1236031c7f28ddfa5b2a21ff
Otomakan <jgmisteli@gmail.com>
2018-11-05 08:35:30 +0900

    The key is in the queue

---
+++ theNatureOfCode/IntelligenceNLearning/Introduction/BreadthFirstSearch/afterWikiCode/sketch.js
@@ -0,0 +1,29 @@
+let graph = {
+  'you': ['alice', 'bob', 'claire'],
+  'bob': ['anuj', 'peggy'],
+  'alice': ['peggy'],
+  'claire': ['thom', 'jonny'],
+  'ron':[],
+  'anuj': [],
+  'peggy': ['ron'],
+  'thom': [],
+  'jonny': [],
+}
+
+let nodeGroup = new AllNodes()
+let nodeRoot= new Node('you', nodeGroup)
+// let group = new Group(graph)
+// function setup(){
+	
+	// createCanvas(600,600);
+	// background(25);
+  nodeRoot.setConnections('you', graph)
+  console.log(nodeRoot)
+	// nodeTree.setChildren('you',graph)
+
+// }
+
+// function draw(){
+	
+// 	// nodeTree.traverse('peggy')
+// }
