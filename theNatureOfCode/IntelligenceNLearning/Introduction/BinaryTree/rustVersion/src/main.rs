4529b181e46b90913650d6d8c8be01edaa583dc7
Otomakan <jgmisteli@gmail.com>
2018-11-07 15:00:05 +0900

    dabbling with types in rust binary tree

--- theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
+++ theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
@@ -1,20 +1,25 @@
-use std::fmt;
+
 
 #![feature(box_syntax, box_patterns)]
-extern crate syntax;
+// extern crate syntax;
+use std::fmt;
 #[derive(Debug)]
+#[derive(PartialEq)]
 // We have to put Node in a box otherwise we get 'recursive has infinite size" error
 // The box isis a smart pointer to a heap allocated value of type T
-struct Node {
-	left: Option<Box<Node>>,
-	right: Option<Box<Node>>,
-	val: i16,
+struct Node<'a> {
+	val: &'a i16,
+	left: Option<Box<Node<'a>>>,
+	right: Option<Box<Node<'a>>>,
 }
+// impl PartialEq for Node {
+//     fn eq(&self, other: &Node) -> bool {
+//         self.val == other.val
+//     }
+// }
 
-
-impl fmt::Display for Node{
+impl <'a>fmt::Display for Node<'a>{
 	fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
-
 		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,self.right)
 	}
 }
@@ -29,16 +34,30 @@ fn main() {
   (3, "ralp"),
   (7,"emm")
   ];
-  let bob = Node {left:None, right:None, val:60_i16};
-   let bob_dad = Node {left:None, right:Some(Box::new(bob)), val:30_i16};
+
+  let mut charles = Node {left:None, right:None, val:&30_i16};
+  let mut bob = Node {left:None, right:Some(Box::new(charles)), val:&60_i16};
+  let mut ray = Node {left:None, right:None, val:&30_i16};
+  
+   let mut bob_dad = Node {left:None, right:Some(Box::new(bob)), val:&30_i16};
    println!("{:?}",my_list);
-   println!("{}",bob_dad );
+   // println!("{}",bob_dad );
 	match bob_dad.right{
-		None => println!("Nope"),
-		Some(val) => println!("{:?}", val),
+		&mut None => println!("Nope"),
+		&mut Some(ref mut subnode) => println!("{:?}", subnode),
+		_ => println!("ero");
 	}
-	match bob.right{
-		None => println!("Nope"),
-		Some(box node) => println!("{:?}", node),
-	}
+	// if bob.right==Some(Box::new(ray)){
+	// 	println!("Noooo");
+	// }
+	// if bob.right==Some(Box::new(charles)){
+	// 	println!("Noooo");
+	// }
+	// else{
+	// 	println!("Yooo");
+	// }
+	// match bob.right{
+	// 	None => println!("Nope"),
+	// 	Some(box node) => println!("{:?}", node),
+	// }
 }
