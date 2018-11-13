94100c20f222c3211cb87dda530b1a0968fca930
Otomakan <jgmisteli@gmail.com>
2018-11-13 18:57:24 +0900

    Rust binary tree

--- theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
+++ theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
@@ -7,10 +7,21 @@ use std::fmt;
 #[derive(PartialEq)]
 // We have to put Node in a box otherwise we get 'recursive has infinite size" error
 // The box isis a smart pointer to a heap allocated value of type T
-struct Node<'a>{
-	val: i16,
-	left: Option<&'a Node<'a>>,
-	right: Option<&'a Node<'a>>,
+
+// type Link = ;
+struct Node<'a> {
+    val: &'a i16,
+    left: Option<Box<Node<'a>>>,
+    right: Option<Box<Node<'a>>>,
+}
+impl<'a> Node<'a>{
+	pub fn new(v: &'a i16,r: Option<Box<Node<'a>>>, l:Option<Box<Node<'a>>>) -> Self{
+		Node {
+			val: v,
+			right: r,
+			left: l,
+		}
+	}
 }
 // impl PartialEq for Node {
 //     fn eq(&self, other: &Node) -> bool {
@@ -20,15 +31,24 @@ struct Node<'a>{
 
 impl<'a>  fmt::Display for Node<'a>{
 	fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
-		 match self.right{
-			Some(x) => println!("We are in{}", x),
-			_ =>  println!("{:?}", "lol"),
+		let result = "";
+		let right =  match self.right{
+			Some(ref x) =>  &x,
+			_ => &Box::new("no node"),
 		};
-		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,self.right)
+		// 	let left =  match self.left{
+		// 	Some(ref x) =>  write!(f,"{:?}",x),
+		// 	_ =>  write!(f,"No node"),
+		// };
+		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,right)
 	}
 }
 
-// impl<'a> fmt::Display for std::options::Node<'a>{
+// fn format_node_to_string(Node<'a>)->&str{
+
+// }
+
+// impl<'a> fmt::Display for Node<'a>{
 // 		fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
 // 		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,self.right)
 // 	}
@@ -42,19 +62,20 @@ impl<'a>  fmt::Display for Node<'a>{
 
 fn main() {
 	let my_list =  [
-  (1,"lool"),
-  (6,"super"),
-  (4,"gor"),
-  (2,"generate"),
-  (5,"super"),
-  (3, "ralp"),
-  (7,"emm")
- ];
-
-  let  charles = Node {left:None, right:None, val:30_i16};
-  let  bob = Node {left:None, right:Some(&charles), val:60_i16};
-  let  ray = Node {left:None, right:None, val:30_i16};
-	let  bob_dad = Node {left:None, right:Some(&bob), val:30_i16};
+	  (1,"lool"),
+	  (6,"super"),
+	  (4,"gor"),
+	  (2,"generate"),
+	  (5,"super"),
+	  (3, "ralp"),
+	  (7,"emm")
+ 	];
+ let  charles = Node::new(&60_i16,None, None);
+  let  bob = Node::new(&30_i16,None, Some(Box::new(charles)));
+  // let  charles = Node::new(left:None, right:None, val:&30_i16);
+  // // let  bob = Node::new({left:None, right:Some(Box(charles)), val:&60_i16});
+ //  let  ray = Node::new({left:None, right:None, val:&30_i16});
+	let  bob_dad = Node::new(&30_i16, Some(Box::new(bob)), None);
 	println!("{}",bob_dad );
 	// match bob_dad.right{
 	// 	 None => println!("Nope"),
