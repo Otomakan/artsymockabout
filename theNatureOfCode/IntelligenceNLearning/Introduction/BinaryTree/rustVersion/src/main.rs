c5a80ad092bda884cdadb61acc6344f5204f0372
Otomakan <jgmisteli@gmail.com>
2018-11-13 22:38:05 +0900

    Actually not hard once you understand it

--- theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
+++ theNatureOfCode/IntelligenceNLearning/Introduction/BinaryTree/rustVersion/src/main.rs
@@ -3,12 +3,25 @@
 #![feature(box_syntax, box_patterns)]
 // extern crate syntax;
 use std::fmt;
-#[derive(Debug)]
 #[derive(PartialEq)]
 // We have to put Node in a box otherwise we get 'recursive has infinite size" error
 // The box isis a smart pointer to a heap allocated value of type T
 
 // type Link = ;
+
+#[derive(Debug)]
+
+// struct NodeTree<'a> {
+// 	root: Node<'a>,
+// 	current: & Node<'a>
+// }
+
+
+// impl<'a> NodeTree<'a>{
+// 	fn find_node(& self, target:&i16){
+// 		println!("Found node");
+// 	}
+// }
 struct Node<'a> {
     val: &'a i16,
     left: Option<Box<Node<'a>>>,
@@ -22,6 +35,44 @@ impl<'a> Node<'a>{
 			left: l,
 		}
 	}
+
+	pub fn insert(&mut self, new_val:&'a i16){
+		if self.val == new_val{
+			println!("We already got that value babe");
+			return
+		}
+		if new_val > self.val  {
+			match self.right{
+				Some(ref mut x) => x.insert(new_val),
+				None => self.right = Some(Box::new(Node::new(new_val, None, None)))
+			}
+		}
+		else if new_val < self.val {
+			match self.left{
+				Some(ref mut x)=> x.insert(new_val),
+				None => self.left = Some(Box::new(Node::new(new_val, None, None)))
+			}
+		}
+		else{
+			println!("There was a problem");
+		}
+	}	
+
+	pub fn find_node(& self, target:& i16)-> Option<Box<&Node<'a>>>{
+		// path.push(target);
+		match self.val {
+			val if val == target => Some(Box::new(self)),
+			val if val >  target => match &self.left {
+					Some(subnode) =>  subnode.find_node(target),
+					None => None,
+				},
+			val if val <  target => match &self.right {
+					Some(subnode) =>  subnode.find_node(target),
+					None => None,
+				},
+			_ => None,
+		}
+	}
 }
 // impl PartialEq for Node {
 //     fn eq(&self, other: &Node) -> bool {
@@ -29,36 +80,7 @@ impl<'a> Node<'a>{
 //     }
 // }
 
-impl<'a>  fmt::Display for Node<'a>{
-	fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
-		let result = "";
-		let right =  match self.right{
-			Some(ref x) =>  &x,
-			_ => &Box::new("no node"),
-		};
-		// 	let left =  match self.left{
-		// 	Some(ref x) =>  write!(f,"{:?}",x),
-		// 	_ =>  write!(f,"No node"),
-		// };
-		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,right)
-	}
-}
 
-// fn format_node_to_string(Node<'a>)->&str{
-
-// }
-
-// impl<'a> fmt::Display for Node<'a>{
-// 		fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
-// 		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,self.right)
-// 	}
-// }
-
-// impl fmt::Display for Node{
-// 	fn fmt(&self,f: &mut fmt::Formatter)-> fmt::Result{
-// 		write!(f,"val:{}, left:{:?}, right:{:?}",self.val,self.left,self.right)
-// 	}
-// }
 
 fn main() {
 	let my_list =  [
@@ -70,13 +92,30 @@ fn main() {
 	  (3, "ralp"),
 	  (7,"emm")
  	];
- let  charles = Node::new(&60_i16,None, None);
-  let  bob = Node::new(&30_i16,None, Some(Box::new(charles)));
-  // let  charles = Node::new(left:None, right:None, val:&30_i16);
-  // // let  bob = Node::new({left:None, right:Some(Box(charles)), val:&60_i16});
- //  let  ray = Node::new({left:None, right:None, val:&30_i16});
-	let  bob_dad = Node::new(&30_i16, Some(Box::new(bob)), None);
-	println!("{}",bob_dad );
+ 	let mut root = Node::new(&1_i16, None, None);
+
+ 	root.insert(&1_i16);
+ 	root.insert(&6_i16);
+ 	root.insert(&4_i16);
+ 	root.insert(&3_i16);
+ // 	let  charles = Node::new(&60_i16,None, None);
+ //  	let  bob = Node::new(&30_i16,None, Some(Box::new(charles)));
+ //  	let  ray = Node::new(&30_i16, None, None, );
+	// let  bob_dad = Node::new(&30_i16, Some(Box::new(bob)), None);
+	println!("{:#?}",root );
+
+	let path_to_node = root.find_node(&10);
+
+	println!("{:?}",path_to_node );
+
+
+	// let node_present = true;
+	// while node_present{
+	// 	if !left && !right{
+	// 		node_present = false;
+	// 	}
+	// }
+
 	// match bob_dad.right{
 	// 	 None => println!("Nope"),
 	// 	Some(subnode) => println!("Bobby is a node {}", subnode),
