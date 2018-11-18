6dd458325f68a7a4817a9a528662d84f1a53cfcc
Otomakan <jgmisteli@gmail.com>
2018-11-18 15:51:07 +0900

    Graph and wall ui+wall removal

--- theNatureOfCode/IntelligenceNLearning/Introduction/MazeGenerator/beforeWatch/recursiveBackTracker.js
+++ theNatureOfCode/IntelligenceNLearning/Introduction/MazeGenerator/beforeWatch/recursiveBackTracker.js
@@ -1,82 +1,127 @@
-let storeX = []
-let storeY = []
-let nodeList = []
+
+
+class Graph{
+	constructor(width,height) {
+	  this.rootNode = new Node(0,0,0)
+	  this.allNodes = []
+	  this.width = width
+	  this.height = height
+	  this.edges = []
+	}
+	createGraph(rootNode){
+		if(rootNode.x>this.width && rootNode.y>this.height){
+			console.log('blo')
+			return
+		}
+		this.allNodes.push(rootNode)
+		let leftExists = false
+		let rightExists = false
+		this.allNodes.forEach((node)=>{
+			if(node.x ==rootNode.x+1 && node.y ==rootNode.y){
+				rootNode.leftChild = node 
+				let edge = new Edge(rootNode, rootNode.leftChild)
+				rootNode.leftChild.parent.push(rootNode)
+				rootNode.leftChild.edges.bottom = edge
+				this.edges.push(edge)
+				leftExists =true
+			}
+			if(node.y == rootNode.y+1 && node.x == rootNode.x){
+				rootNode.rightChild = node
+				let edge = new Edge(rootNode, rootNode.rightChild)
+				rootNode.rightChild.parent.push(rootNode)
+				rootNode.rightChild.edges.bottom = edge
+				this.edges.push(edge)
+				rightExists=true
+			}
+		})
+		if(rootNode.x<this.width && !leftExists){
+			rootNode.leftChild = new Node(rootNode.x+1,rootNode.y,rootNode)
+			// this.leftChild.display()
+			let edge = new Edge(rootNode, rootNode.leftChild)
+			rootNode.leftChild.edges.top = edge
+			rootNode.edges.left = edge
+			this.edges.push(edge)
+			this.createGraph(rootNode.leftChild)
+			
+		}
+		if(rootNode.y<this.height && !rightExists){
+			rootNode.rightChild = new Node(rootNode.x, rootNode.y+1,rootNode)
+			let edge = new Edge(rootNode, rootNode.rightChild)
+			rootNode.rightChild.edges.top = edge
+			rootNode.edges.right = edge
+			this.edges.push(edge)
+			// this.rightChild.display
+			this.createGraph(rootNode.rightChild)
+		}
+		
+		// else 
+		// 	return
+	}
+	refreshGraph(){
+
+	}
+
+	removeEdge(edge){
+		// edge = null
+		this.edges[((this.edges.findIndex((e)=> e==edge)))] = null
+		// bob = null
+		edge = null
+	}
+}
+class Edge{
+	constructor(parent,child){
+		this.parent = parent
+		this.child = child
+	}
+	display(){
+		push()
+		stroke(250)
+		strokeWeight(2)
+		line(20+this.child.x*100,20+this.child.y*100,20+this.parent.x*100+100,20+this.parent.y*100+100)
+	
+		pop()
+	}
+}
 class Node  {
-	constructor(x,y) {
+	constructor(x,y,parent) {
 		this.visited = 0
 		this.x = x
 		this.y = y
 		// this.edges
-		this.parent;
-		this.rightChild;
+		this.parent =[parent]
+		this.rightChild
 		this.leftChild
+		this.edges = {
+			top:null,
+			right:null,
+			bottom:null,
+			left:null
+		}
 	}
-	createChildren(w,h){
-		console.log('recursing')
-		if(this.x>=w && this.y>=h){
-			console.log('blo')
-			return
-		}
-		nodeList.push(this)
-		nodeList.forEach((node)=>{
-			console.log(node)
-		})
-
-		if(this.x<w){
-			this.leftChild = new Node(this.x+1,this.y)
-			// this.leftChild.display()
-			this.leftChild.createChildren(w,h)
-			
-		}
-		if(this.y<h){
-			this.rightChild = new Node(this.x, this.y+1)
-			// this.rightChild.display
-			this.rightChild.createChildren(w,h)
-		}
-		// else 
-		// 	return
+	createChildren(w,h, nodeList){
+		
 	}
-	display(){
+	displayNode(){
 		push()
 		fill(255,0,0)
-		ellipse(this.x*10,this.y,10*10)
+		stroke(0)
+		strokeWeight(2)
+		rect(20+this.x*100,20+this.y*100,100,100)
+
+		point(20+this.x*100,20+this.y*100)
+		
+		pop()
+	}
+	displayWalls(){
+		push()
+		stroke(250)
+		strokeWeight(2)
+		if(this.rightChild)
+			line(20+this.rightChild.x*100+100,20+this.rightChild.y*100, 20+this.rightChild.x*100+100,20+this.rightChild.y*100+100)
+		if(this.leftChild){
+			stroke(0,234,0)
+			line(20+this.leftChild.x*100,20+this.leftChild.y*100+100, 20+this.leftChild.x*100+100+100,20+this.leftChild.y*100)
+		}
 		pop()
 	}
 }
-
-// class Edge {
-// 	constructor(props) {
-// 	  super(props);
-// 		this.open = 0
-// 	  this.state = {};
-// 	}
-// }
-
-let gridWidth = 30
-let gridHeight = 25
-// let grid = new Grid(gridWidth,gridHeight)
-
-let pathExplored = []
-
-createGraph(3,3)
-function createGraph(w,h){
-	let rootNode = new Node(0,0)
-	rootNode.createChildren(3,3)
-	console.log(rootNode)
-	console.log(nodeList)
-	// console.log(s)
-	// for (let i = 0; i<w; i++){
-	// 	for(let ii=0; ii<h;ii++){
-
-	// 	}
-	// }
-}
-// for (let i = 0; i<gridWidth; i++){
-// 	grid.cells.push([])
-// 	for(let ii=0; ii<gridHeight;ii++){
-// 		grid.cells[i][ii]= new Cell()	
-// 		if(i>1){
-
-// 		}
-// 	}
-// }
--- theNatureOfCode/IntelligenceNLearning/Introduction/MazeGenerator/beforeWatch/sketch.js
+++ theNatureOfCode/IntelligenceNLearning/Introduction/MazeGenerator/beforeWatch/sketch.js
@@ -3,52 +3,59 @@
 // for red, green, and blue color values
 // var r, g, b;
 
-let boxWidth;
-let boxHeight;
 
+  var myGraph = new Graph(3,3)
 function setup() {
-  createCanvas(grid.width*10, grid.height*10);
+  createCanvas(600, 600);
   // Pick colors randomly
   r = random(255);
   g = random(255);
   b = random(255);
-  console.log(grid)
+  // console.log(grid)
 
-  let xrand = floor(random(0, grid.width))
-  let yrand = floor(random(0,grid.height))
+  // let xrand = floor(random(0, grid.width))
+  // let yrand = floor(random(0,grid.height))
 
-   grid.cells[xrand][yrand].maze = 1
-  grid.addAdjacentWalls(xrand,yrand)
+  //  grid.cells[xrand][yrand].maze = 1
+  // grid.addAdjacentWalls(xrand,yrand)
+  myGraph.createGraph(myGraph.rootNode)
 
 }
 
 function draw() {
   background(51);
-  // Draw a circle
-  strokeWeight(2);
-  noStroke();
-  rect(0,0,grid.width*10,grid.height*10)
+  // rect(0,0,grid.width*10,grid.height*10)
 
-  if(grid.wallsList.length != 0){
-    let randomWall = floor(random(0, grid.wallsList.length))
-    let selectedWall = this.wallsList(randomList)
-    grid.addAdjacentWalls(selectedWall[0],selectedWall[1])
-    // let yrand = floor(random(0,grid.wallsList.length))
+  // if(grid.wallsList.length != 0){
+  //   let randomWall = floor(random(0, grid.wallsList.length))
+  //   let selectedWall = this.wallsList(randomList)
+  //   grid.addAdjacentWalls(selectedWall[0],selectedWall[1])
+  //   // let yrand = floor(random(0,grid.wallsList.length))
 
-    // grid.cells[xrand][yrand].maze = 1
-    // grid.addAdjacentWalls(xrand,yrand)
-  }
+  //   // grid.cells[xrand][yrand].maze = 1
+  //   // grid.addAdjacentWalls(xrand,yrand)
+  // }
+  myGraph.allNodes.forEach(function (node,it) {
+    node.displayNode()
+  })
+   myGraph.edges.forEach(function (edge,it) {
+    
+    if(edge == null)
+      return
+    edge.display()
+  });
+  // console.log(rootNode)
+  // console.log(nodeList)
 
-
-  for (let i=0; i<grid.width;i++){
-    for(let ii=0; ii<grid.height;ii++){
-      pop();
-      if(grid.cells[i][ii].maze==1)
-        fill(240)
-      else
-        fill(0)
-      rect(i*10,ii*10, 10,10)
-      push();
-    }
-  }
+  // for (let i=0; i<grid.width;i++){
+  //   for(let ii=0; ii<grid.height;ii++){
+  //     pop();
+  //     if(grid.cells[i][ii].maze==1)
+  //       fill(240)
+  //     else
+  //       fill(0)
+  //     rect(i*10,ii*10, 10,10)
+  //     push();
+  //   }
+  // }
 }
