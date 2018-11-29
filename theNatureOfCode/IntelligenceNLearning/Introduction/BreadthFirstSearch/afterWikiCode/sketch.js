c3d582a2fb55c4cbcffc90100d71c71ebabfb8db
Otomakan <jgmisteli@gmail.com>
2018-11-29 19:02:01 +0900

    It's not that hard to do BFS if you take you take a step back

--- theNatureOfCode/IntelligenceNLearning/Introduction/BreadthFirstSearch/afterWikiCode/sketch.js
+++ theNatureOfCode/IntelligenceNLearning/Introduction/BreadthFirstSearch/afterWikiCode/sketch.js
@@ -10,15 +10,42 @@ let graph = {
   'jonny': [],
 }
 
-let nodeGroup = new AllNodes()
-let nodeRoot= new Node('you', nodeGroup)
-// let group = new Group(graph)
-// function setup(){
+// let nodeGroup = new AllNodes()
+// let nodeRoot= new Node('you', nodeGroup)
+// // let group = new Group(graph)
+// // function setup(){
 	
-	// createCanvas(600,600);
-	// background(25);
-  nodeRoot.setConnections('you', graph)
-  console.log(nodeRoot)
+// 	// createCanvas(600,600);
+// 	// background(25);
+//   nodeRoot.setConnections('you', graph)
+//   console.log(nodeRoot)
+target='ron'
+
+  // function exploreGraph(graph,rootEl,target){
+    history = []
+    visitedNodes = []
+    FIFOqueue = []
+    FIFOqueue.push('you')
+    while (FIFOqueue.length>0){
+      currentNode =FIFOqueue[0]
+      console.log(`visiting ${currentNode}`)
+      if(target==currentNode){
+        console.log('found')
+        FIFOqueue = [currentNode]
+      }
+      history.push()
+      graph[currentNode].forEach((el) =>{
+        if(!visitedNodes.includes(el)){
+          FIFOqueue.push(el)
+          visitedNodes.push(el)
+        }
+      })
+      FIFOqueue.shift()
+    }
+    console.log(FIFOqueue)
+    console.log()
+  // }
+  // exploreGraph(graph, 'you')
 	// nodeTree.setChildren('you',graph)
 
 // }
