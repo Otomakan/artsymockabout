dc13daac2b25d0e4ab9a63649ed97fafcdfdc1ff
Otomakan <jgmisteli@gmail.com>
2019-03-07 13:12:13 +0900

    Proper shibuya order

--- random/dancing_shibuyalines/firstTry/sketch.js
+++ random/dancing_shibuyalines/firstTry/sketch.js
@@ -1,28 +1,41 @@
-const lineDotNum = 200
+const lineDotNum = 80
+const dotWidth = 10
+const recLength = 70
+const distanceEach  =dotWidth*3+recLength+recLength
+const arrLineDots = []
 function setup(){
 	createCanvas(600,600)
 	background(0)
-	arrLineDots = []
 	let y=20
 	let x =0 
+	let cross = false
 	for(let i = 1; i < lineDotNum; i++){
-		arrLineDots.push(new LineDot(x, y,240))
-		if(i*80%600===0){
-			print(y)
-			y+=80
+
+		if(cross)
+			arrLineDots.push(new LineDot(x, y,240))
+		else
+			arrLineDots.push(new DotLine(x, y,240))
+		if(i*distanceEach%25===0){
+			console.log(y)
+			y+=dotWidth*2
 		}
-		x+=80
-		if(i*80%600===0){
-			print(y)
-			x=0
+		x+=distanceEach
+		if(i*distanceEach%25===0){
+			if(cross)
+				x=0
+			else
+				x=-recLength
+			cross = !cross
 		}
 	}
+	console.log(arrLineDots)
+	console.log(arrLineDots.length)
 }
 
 function draw(){
 	// translate(width/2, height/16)
 	// rotate(PI / 3.0)
-	for(let i = 0; i < lineDotNum; i++){
+	for(let i = 0; i < lineDotNum-1; i++){
 		arrLineDots[i].draw()
 	}
 }
@@ -36,8 +49,22 @@ class LineDot {
 	draw(){
 		stroke(0)
 		fill(this.color)
-		rect(this.xpos, this.ypos, 50,5)
-		rect(this.xpos+65, this.ypos, 5,5)
+		rect(this.xpos, this.ypos, recLength,dotWidth)
+		rect(this.xpos+recLength+dotWidth, this.ypos, dotWidth,dotWidth)
+	}
+	// methods
+}
+class DotLine {
+	constructor(xpos, ypos, color) {
+		this.xpos = xpos
+		this.ypos = ypos
+		this.color = color
+	}
+	draw(){
+		stroke(0)
+		fill(this.color)
+		rect(this.xpos+dotWidth, this.ypos, recLength,dotWidth)
+		rect(this.xpos-dotWidth, this.ypos, dotWidth,dotWidth)
 	}
 	// methods
 }
