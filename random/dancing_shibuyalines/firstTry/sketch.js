4496c1e562569aacbe4e29b9d440126ac68f077c
Otomakan <jgmisteli@gmail.com>
2019-03-08 22:54:15 +0900

    Tripy scale effect

--- random/dancing_shibuyalines/firstTry/sketch.js
+++ random/dancing_shibuyalines/firstTry/sketch.js
@@ -1,28 +1,36 @@
-const lineDotNum = 80
+const lineDotNum = 120
 const dotWidth = 10
 const recLength = 70
 const distanceEach  =dotWidth*3+recLength+recLength
 const arrLineDots = []
 const width = 600
 const height = 600
+const backgroundColor = 0
+let scaleRate = 0.0001
+let scaleRatio = 1
 function setup(){
+
+const stickColor = color(50,200,80)
 	createCanvas(width,height)
 	background(0)
 	let y=20
 	let x =0 
 	let cross = false
+	let linePerRow  = Math.ceil(width/distanceEach)
+	// console.log(Math.ceil(linePerRow))
 	for(let i = 1; i < lineDotNum; i++){
 
 		if(cross)
-			arrLineDots.push(new LineDot(x, y,240))
+			arrLineDots.push(new LineDot(x, y,stickColor))
 		else
-			arrLineDots.push(new DotLine(x, y,240))
-		if(i*distanceEach%25===0){
+			arrLineDots.push(new DotLine(x, y,stickColor))
+		
+		x+=distanceEach
+		if(i%linePerRow===0){
 			console.log(y)
 			y+=dotWidth*2
 		}
-		x+=distanceEach
-		if(i*distanceEach%25===0){
+		if(i%linePerRow===0){
 			if(cross)
 				x=0
 			else
@@ -35,14 +43,21 @@ function setup(){
 }
 
 function draw(){
-	// translate(width/2, height/16)
+	// translate(width/2, height/2)
 	// rotate(PI / 3.0)
+
+	// scale(scaleRatio)
+	background(0)
+	// scale(1)
 	for(let i = 0; i < lineDotNum-1; i++){
 		let tempObj = arrLineDots[i]
 		if(tempObj instanceof LineDot)
 			tempObj.move()
+		if(tempObj instanceof DotLine)
+			tempObj.move()
 		tempObj.draw()
 	}
+	scaleRatio += scaleRate
 }
 
 class LineDot {
@@ -70,8 +85,14 @@ class DotLine {
 		this.ypos = ypos
 		this.color = color
 	}
+	move(){
+		this.xpos-=1
+		if(this.xpos<-recLength-dotWidth*2)
+			this.xpos=width
+	}
 	draw(){
 		stroke(0)
+		scale(scaleRatio)
 		fill(this.color)
 		rect(this.xpos+dotWidth, this.ypos, recLength,dotWidth)
 		rect(this.xpos-dotWidth, this.ypos, dotWidth,dotWidth)
