1615780ce9ace1e2af4474a6da0f53d993ce399e
Otomakan <jgmisteli@gmail.com>
2019-05-23 09:07:47 +0900

    adding steering behavior

--- theNatureOfCode/IntelligenceNLearning/2GeneticAlgo/steeringBehavior/assets.js
+++ theNatureOfCode/IntelligenceNLearning/2GeneticAlgo/steeringBehavior/assets.js
@@ -1,13 +1,70 @@
-
+class Vector{
+	constructor(x, y){
+		this.x = x
+		this.y = y
+	}
+	add(otherVector){
+		return new Vector(this.x + otherVector.x, this.y + otherVector.y)
+	}
+}
 class Vehicule {
-	constructor(width, height, itemSize){
-	
+	constructor(x, y, acceleration, velocity, health){
+		this.loc = new Vector(x, y)
+		this.acceleration = acceleration
+		this.velocity  = velocity
+		this.health = health
 	}
-	mazeToGraph(){
+
+	display(){
+		noFill()
+		stroke(0)
+		strokeWeight(2)
+		rect(this.loc.x, this.loc.y, 10,10)
+	}
+	move(){
+		this.loc = this.loc.add(this.velocity)
+	}
+	correctiveFunction(){
 
 	}
-	
-	display(){
+	steer(){
+
+	}
+	lookAround(foodArray,poisonArray){
+		foodArray.forEach((food)=>{
+			noFill()
+			stroke(0)
+			strokeWeight(1)
+			ellipse(this.loc.x, this.loc.y, 60,60)
+			// dx = abs(x-center_x)
+			// dy = abs(y-center_y)
+			// R = radius
+			if (pow(abs(food.x - this.loc.x),2) + pow(abs(food.y - this.loc.y),2) < pow(30,2)){
+				
+				ellipse(food.x,food.y, 10,10)
+				const dx = (this.loc.x - food.x)/(this.loc.y - food.y)
+				const dy = (this.loc.y - food.y)/(this.loc.x - food.x)
+				this.velocity.x  = dx
+				this.velocity.y  = dy
+
+			}
+		})
+		// poisonArray.forEach((poison)=>{
+		// 	noFill()
+		// 	stroke(0)
+		// 	strokeWeight(1)
+		// 	ellipse(this.loc.x, this.loc.y, 60,60)
+		// 	// dx = abs(x-center_x)
+		// 	// dy = abs(y-center_y)
+		// 	// R = radius
+		// 	if (pow(abs(poison.x - this.loc.x),2) + pow(abs(poison.y - this.loc.y),2) < pow(30,2)){
+				
+		// 		ellipse(poison.x,poison.y, 10,10)
+		// 		this.velocity.x  = (this.loc.x - poison.x)/(this.loc.y - poison.y)
+		// 		this.velocity.y  = (this.loc.y - poison.y)/(this.loc.x - poison.x)
+
+		// 	}
+		// })
 	}
 
 }
--- theNatureOfCode/IntelligenceNLearning/2GeneticAlgo/steeringBehavior/main.js
+++ theNatureOfCode/IntelligenceNLearning/2GeneticAlgo/steeringBehavior/main.js
@@ -3,19 +3,32 @@
 
 let poisons = []
 let foods = []
+let vehicules = []
 let redDot = new Food(100,100,30)
+const getRandomX = () => random(0,640)
+const getRandomY = () => random(0,480)
+const getRandomVelocity = () => new Vector(random(-1,1), random(-1,1))
+
 function setup() {
   createCanvas(640, 480)
-    for(let i = 0; i < 30; i++){
-        poisons.push(new Poison(random(0,640), random(0,480), 5))
+    for(let i = 0; i < 30; i++) {
+        poisons.push(new Poison(getRandomX(), getRandomY(), 5))
     }
-    for(let i = 0; i < 50; i++){
-        foods.push(new Food(random(0,640), random(0,480), 5))
+    for(let i = 0; i < 50; i++) {
+        foods.push(new Food(getRandomX(), getRandomY(), 5))
     }
+    for(let i = 0; i < 5; i++) {
+        const defaultHealth = 10
+        const defaultAcceleration = 0.01
+
+        vehicules.push(new Vehicule(getRandomX(), getRandomY(), defaultAcceleration, getRandomVelocity(), defaultHealth))
+    }
+    frameRate(10)
 }
 function draw() {
     background(252, 245, 194)
     poisons.forEach((poison)=>{poison.display()})
     foods.forEach((food)=>{food.display()})
+    vehicules.forEach((vehicule)=>{vehicule.display();vehicule.move();vehicule.lookAround(foods, poisons)})
 }
 
